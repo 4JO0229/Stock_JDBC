28523ce4c456a0bd4971b65f12a6d6c0900fd89a
LXXDJ <joker96.dj@gmail.com>
2024-03-05 22:25:21 +0900

    finish

---
+++ src/main/java/com/ohgiraffers/stock/controller/deleteController.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers.stock.controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.ohgiraffers.stock.common.JDBCTemplate.close;
+import static com.ohgiraffers.stock.common.JDBCTemplate.getConnection;
+
+public class deleteController {
+    public int deleteStock(String name) {
+        Connection con = getConnection();
+        PreparedStatement pstmt = null;
+        int result = 0;
+        Properties prop = new Properties();
+
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/stock/mapper/stock-query.xml"));
+            String query = prop.getProperty("deleteStock");
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, name);
+            result = pstmt.executeUpdate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(pstmt);
+            close(con);
+        }
+
+        return result;
+    }
+}
